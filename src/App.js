96cf51b3e9004a3aae51d17e9f5e224220505598
vyasishanatc194 <vyasishanatc194@gmail.com>
2022-02-24 16:28:56 +0530

    Event Handling using functional component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 import Hello from './components/Hello'
+import FunctionClick  from './components/FunctionClick';
+
 function App() {
   return (
     <div className="App">
-        <Greet name="Bruce" heroName="Batman">
+        {/* <Greet name="Bruce" heroName="Batman">
           <p>This is children props</p>
           </Greet>
         <Greet name="Clark" heroName="Superman">
           <button>Action</button>
           </Greet>
-        <Greet name="Diana" heroName="Wonder Woman"/>
+        <Greet name="Diana" heroName="Wonder Woman"/> */}
         {/* <Hello /> */}
+        <FunctionClick/>
     </div>
   );
 }
